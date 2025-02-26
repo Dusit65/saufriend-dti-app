9f2810a6a1febf7c2dfcb19188d9976c540a7f64
Dusit Pakdekunakorn <S6552410011@sau.ac.th>
2025-02-26 23:12:44 +0700

    Developing

--- src/views/MyFriend.jsx
+++ src/views/MyFriend.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import { React, useEffect, useState } from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Avatar,
+} from "@mui/material";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
+import Profile from "./../assets/profile.png";
+import { Link } from "react-router-dom";
 function MyFriend() {
   return (
     <div>MyFriend</div>
